8decb6c963f0f4c0ae5958c8fde4c739fc68ca0d
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-01-07 15:09:33 +0000

    note show page

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,10 +1,17 @@
 class Public::FavoritesController < ApplicationController
   def create
+    note = Note.find(params[:note_id])
+    @favorite = current_customer.favorites.new(note_id: note.id)
+    @favorite.save
   end
 
   def destroy
+    note = Note.find(params[:note_id])
+    @favorite = current_customer.favorites.find_by(note_id: note.id)
+    @favorite.destroy
   end
 
   def favorite
+    @favorites = current_customer.favorites.order(created_at: :desc)
   end
 end
--- app/controllers/public/note_comments_controller.rb
+++ app/controllers/public/note_comments_controller.rb
@@ -1,7 +1,19 @@
 class Public::NoteCommentsController < ApplicationController
   def create
+    @note = Note.find(params[:note_id])
+    comment = current_customer.note_comments.new(note_comment_params)
+    comment.note_id = @note.id
+    comment.save
   end
 
   def destroy
+    @note = Note.find(params[:note_id])
+    NoteComment.find(params[:id]).destroy
+  end
+
+  private
+
+  def note_comment_params
+    params.require(:note_comment).permit(:comment,:post_status)
   end
 end
