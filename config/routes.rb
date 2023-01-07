55ca2bad318898d11a5ba70499ec835470831f85
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-01-07 13:27:01 +0000

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+   devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
   root to: "public/homes#top"
   get "about" => "public/homes#about"
   get "search" => "public/searches#search"
@@ -19,10 +24,6 @@ Rails.application.routes.draw do
   resources :categories, only: [:create,:destroy]
 
 
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
 
   devise_for :admin,skip: [:passwords,:registrations], controllers: {
     sessions: "admin/sessions"
