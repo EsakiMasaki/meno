48436cc00509336fdf737505f0c73246510f6523
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-01-08 05:04:03 +0000

    category

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -1,2 +1,19 @@
 class Public::CategoriesController < ApplicationController
+  def create
+    category = current_customer.categories.new(category_params)
+    category.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    category = current_customer.categories.find(params[:id])
+    category.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
