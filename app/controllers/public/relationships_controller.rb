112c377e66778081de80c41d1ebf419dea998217
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2023-01-12 06:46:40 +0000

    follow paginate

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -15,11 +15,11 @@ class Public::RelationshipsController < ApplicationController
 
   def followers
     @customer = Customer.find(params[:customer_id])
-    @customers = @customer.followers
+    @customers = @customer.followers.page(params[:page])
   end
 
   def followings
     @customer = Customer.find(params[:customer_id])
-    @customers = @customer.followings
+    @customers = @customer.followings.page(params[:page])
   end
 end
